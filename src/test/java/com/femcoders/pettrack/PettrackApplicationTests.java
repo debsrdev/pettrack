317e9dd0a743a6d46caa200d09ad3d1d072780e0
Débora Rubio <debora.rubio.soliva@gmail.com>
2025-08-06 10:32:26 +0200

    first commit

---
+++ src/test/java/com/femcoders/pettrack/PettrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.pettrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
