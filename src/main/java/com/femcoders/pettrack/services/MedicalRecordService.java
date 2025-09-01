6693666cca6556507d44136a3fc66e97cd098b6b
Débora Rubio <debora.rubio.soliva@gmail.com>
2025-09-01 16:53:05 +0200

    feat(MedicalRecordController, MedicalRecordService, MedicalRecordServiceImpl) add logic to get medical record by id with role based access control

--- src/main/java/com/femcoders/pettrack/services/MedicalRecordService.java
+++ src/main/java/com/femcoders/pettrack/services/MedicalRecordService.java
@@ -1,10 +1,12 @@
 package com.femcoders.pettrack.services;
 
 import com.femcoders.pettrack.dtos.medicalRecord.MedicalRecordResponse;
+import com.femcoders.pettrack.models.User;
 import com.femcoders.pettrack.security.UserDetail;
 
 import java.util.List;
 
 public interface MedicalRecordService {
     List<MedicalRecordResponse> GetAllMedicalRecords(UserDetail userDetail);
+    MedicalRecordResponse GetMedicalRecordById(Long id, UserDetail userDetail);
 }
